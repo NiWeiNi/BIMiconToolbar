8dc5168206338c09c483d3da45a4bbd12e0ad3d5
NiWeiNi <weinini007@gmail.com>
2020-09-24 11:43:50 +1000

    refactor: number windows and doors share helper code

--- Helpers/Helpers.cs
+++ Helpers/Helpers.cs
@@ -1,4 +1,5 @@
 ﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -23,38 +24,102 @@ namespace BIMiconToolbar.Helpers
             }
         }
 
-        public static void InstanceFromToRoom(ref Dictionary<string, int> instanceInRoomCount,
+        public static void numberFamilyInstance(Document doc,
+                                                BuiltInCategory builtInCategory)
+        {
+            // Create dictionary to store window-room values
+            Dictionary<FamilyInstance, string> instanceNumbers = new Dictionary<FamilyInstance, string>();
+            // Create dictionary to store number of windows in room
+            Dictionary<string, int> instancesInRoomCount = new Dictionary<string, int>();
+
+            // Design option filter
+            ElementDesignOptionFilter designOptionFilter = new ElementDesignOptionFilter(ElementId.InvalidElementId);
+
+            // Select phase
+            PhaseArray aPhase = doc.Phases;
+            Phase phase = aPhase.get_Item(aPhase.Size - 1);
+
+            ElementId idPhase = phase.Id;
+            ParameterValueProvider provider = new ParameterValueProvider(new ElementId((int)BuiltInParameter.PHASE_CREATED));
+            FilterNumericRuleEvaluator evaluator = new FilterNumericEquals();
+            FilterElementIdRule rule = new FilterElementIdRule(provider, evaluator, idPhase);
+            ElementParameterFilter paraFilter = new ElementParameterFilter(rule);
+
+            // Collect all windows in project
+            using (FilteredElementCollector instances = new FilteredElementCollector(doc).OfCategory(builtInCategory)
+                                            .WhereElementIsNotElementType().WherePasses(designOptionFilter).WherePasses(paraFilter))
+            {
+                // Retrieve rooms from windows
+                foreach (FamilyInstance inst in instances)
+                {
+                    if (inst.ToRoom != null)
+                    {
+                        string roomNumber = inst.ToRoom.Number;
+                        Helpers.InstanceFromToRoom(instancesInRoomCount, roomNumber, instanceNumbers, inst);
+                    }
+                    else if (inst.FromRoom != null)
+                    {
+                        string roomNumber = inst.FromRoom.Number;
+                        Helpers.InstanceFromToRoom(instancesInRoomCount, roomNumber, instanceNumbers, inst);
+                    }
+                }
+            }
+
+            // Create transaction and make changes in Revit
+            using (Transaction t = new Transaction(doc, "Number Instances"))
+            {
+                t.Start();
+
+                // Empty Mark parameter to avoid duplicated values
+                foreach (KeyValuePair<FamilyInstance, string> entry in instanceNumbers)
+                {
+                    Parameter instanceMark = entry.Key.get_Parameter(BuiltInParameter.ALL_MODEL_MARK);
+                    instanceMark.Set("");
+                }
+
+                // Populate Mark parameter
+                foreach (KeyValuePair<FamilyInstance, string> entry in instanceNumbers)
+                {
+                    Parameter instanceMark = entry.Key.get_Parameter(BuiltInParameter.ALL_MODEL_MARK);
+                    instanceMark.Set(entry.Value);
+                }
+
+                t.Commit();
+            }
+        }
+
+        public static void InstanceFromToRoom(Dictionary<string, int> instanceInRoomCount,
                                               string roomNumber,
-                                              ref Dictionary<FamilyInstance, string> instanceNumbers,
+                                              Dictionary<FamilyInstance, string> instanceNumbers,
                                               FamilyInstance familyInstance)
         {
             // Letters to map from integers
             const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-            // Classify door
+            // Classify family instances
             if (instanceInRoomCount.ContainsKey(roomNumber))
             {
-                // Check door count is one, a prefix will be added to the number
+                // Check instance count is one, a prefix will be added to the number
                 if (instanceInRoomCount[roomNumber] == 1)
                 {
-                    // Retrieve door with single count from the dictionary that stores door-room relationship
-                    FamilyInstance keyDoor = instanceNumbers.FirstOrDefault(x => x.Value == roomNumber).Key;
-                    // Change single count door prefix
-                    instanceNumbers[keyDoor] = roomNumber + "-" + letters[instanceInRoomCount[roomNumber] - 1];
-                    // Store current door and room in dict
+                    // Retrieve instance with single count from the dictionary that stores instance-room relationship
+                    FamilyInstance keyInstance = instanceNumbers.FirstOrDefault(x => x.Value == roomNumber).Key;
+                    // Change single count instance prefix
+                    instanceNumbers[keyInstance] = roomNumber + "-" + letters[instanceInRoomCount[roomNumber] - 1];
+                    // Store current instance and room in dict
                     instanceNumbers[familyInstance] = roomNumber + "-" + letters[instanceInRoomCount[roomNumber]];
                 }
                 else
                 {
-                    // Store current door and room in dict
+                    // Store current instance and room in dict
                     instanceNumbers[familyInstance] = roomNumber + "-" + letters[instanceInRoomCount[roomNumber]];
                 }
-                // Increase the door count in room
+                // Increase the instance count in room
                 instanceInRoomCount[roomNumber] = instanceInRoomCount[roomNumber] + 1;
             }
             else
             {
-                // No door is in room , add door-room to dict
+                // No instance is in room , add instance-room to dict
                 instanceNumbers.Add(familyInstance, roomNumber);
                 instanceInRoomCount.Add(roomNumber, 1);
             }
--- NumberDoors/NumberDoors.cs
+++ NumberDoors/NumberDoors.cs
@@ -1,8 +1,6 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace BIMiconToolbar.NumberDoors
 {
@@ -12,90 +10,13 @@ namespace BIMiconToolbar.NumberDoors
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             Document doc = commandData.Application.ActiveUIDocument.Document;
+            BuiltInCategory builtInCategory = BuiltInCategory.OST_Doors;
 
-            // Create dictionary to store door-room values
-            Dictionary<FamilyInstance, string> doorNumbers = new Dictionary<FamilyInstance, string>();
-            // Create dictionary to store number of doors in room
-            Dictionary<string, int> doorsInRoomCount = new Dictionary<string, int>();
+            Helpers.Helpers.numberFamilyInstance(doc, builtInCategory);
 
-            // Collect all doors in project
-            using (FilteredElementCollector doors = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Doors)
-                                            .WhereElementIsNotElementType())
-            {
-                // Retrieve rooms from doors
-                foreach (FamilyInstance door in doors)
-                {
-                    if (door.ToRoom != null)
-                    {
-                        string roomNumber = door.ToRoom.Number;
-                        ClassifyDoors(doorsInRoomCount, roomNumber, doorNumbers, door);
-                    }
-                    else if (door.FromRoom != null)
-                    {
-                        string roomNumber = door.FromRoom.Number;
-                        ClassifyDoors(doorsInRoomCount, roomNumber, doorNumbers, door);
-                    }
-                }
-            }
-
-            // Create transaction and make changes in Revit
-            using (Transaction t = new Transaction(doc, "Number Doors"))
-            {
-                t.Start();
-
-                // Empty Mark parameter to avoid duplicated values
-                foreach (KeyValuePair<FamilyInstance, string> entry in doorNumbers)
-                {
-                    Parameter doorMark = entry.Key.get_Parameter(BuiltInParameter.DOOR_NUMBER);
-                    doorMark.Set("");
-                }
-
-                // Populate door numbers in Mark parameter
-                foreach (KeyValuePair<FamilyInstance, string> entry in doorNumbers)
-                {
-                    Parameter doorMark = entry.Key.get_Parameter(BuiltInParameter.DOOR_NUMBER);
-                    doorMark.Set(entry.Value);
-                }
-
-                t.Commit();
-            }
+            TaskDialog.Show("Number Doors", "Doors numbered");
 
             return Result.Succeeded;
-        }
-
-        private void ClassifyDoors(Dictionary<string, int> doorsInRoomCount, string roomNumber,
-                     Dictionary<FamilyInstance, string> doorNumbers, FamilyInstance door)
-        {
-            // Letters to map from integers
-            const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-            // Classify door
-            if (doorsInRoomCount.ContainsKey(roomNumber))
-            {
-                // Check door count is one, a prefix will be added to the number
-                if (doorsInRoomCount[roomNumber] == 1)
-                {
-                    // Retrieve door with single count from the dictionary that stores door-room relationship
-                    FamilyInstance keyDoor = doorNumbers.FirstOrDefault(x => x.Value == roomNumber).Key;
-                    // Change single count door prefix
-                    doorNumbers[keyDoor] = roomNumber + "-" + letters[doorsInRoomCount[roomNumber] - 1];
-                    // Store current door and room in dict
-                    doorNumbers[door] = roomNumber + "-" + letters[doorsInRoomCount[roomNumber]];
-                }
-                else
-                {
-                    // Store current door and room in dict
-                    doorNumbers[door] = roomNumber + "-" + letters[doorsInRoomCount[roomNumber]];
-                }
-                // Increase the door count in room
-                doorsInRoomCount[roomNumber] = doorsInRoomCount[roomNumber] + 1;
-            }
-            else
-            {
-                // No door is in room , add door-room to dict
-                doorNumbers.Add(door, roomNumber);
-                doorsInRoomCount.Add(roomNumber, 1);
-            }
         }
     }
 }
