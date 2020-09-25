bd49c26c765a804a5414c29c11ffbaf56d757dea
NiWeiNi <weinini007@gmail.com>
2020-09-25 12:51:23 +1000

    feat: add wpf browser window

---
+++ Helpers/Browser/Browser.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BIMiconToolbar.Helpers.Browser
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class BrowserWindow : Window, IDisposable 
+    {
+        public BrowserWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Dispose()
+        {
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach(var drive in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem() 
+                {
+                    Header = drive,
+                    Tag = drive
+                };
+
+                item.Items.Add(null);
+
+                // Listen for item expansion
+                item.Expanded += Item_Expanded;
+
+                FolderView.Items.Add(item);
+            }
+        }
+
+        #region Folder expanded
+        private void Item_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+
+            if (item.Items.Count != 1 || item.Items[0] != null)
+            {
+                return;
+            }
+
+            item.Items.Clear();
+
+            var fullPath = (string)item.Tag;
+
+            var directories = new List<string>();
+
+            try
+            {
+                var dirs = Directory.GetDirectories(fullPath);
+                
+                if (dirs.Length > 0)
+                {
+                    directories.AddRange(dirs);
+                }
+            }
+            catch
+            {
+
+            }
+
+            directories.ForEach(directoryPath =>
+            {
+                var subItem = new TreeViewItem()
+                {
+                    Header = GetFileFolderName(directoryPath),
+                    Tag = directoryPath
+                };
+
+                subItem.Items.Add(null);
+
+                subItem.Expanded += Item_Expanded;
+
+                item.Items.Add(subItem);
+            });
+
+            #region Get files
+
+            var files = new List<string>();
+
+            try
+            {
+                var fs = Directory.GetFiles(fullPath);
+
+                if (fs.Length > 0)
+                {
+                    files.AddRange(fs);
+                }
+            }
+            catch
+            {
+
+            }
+
+            files.ForEach(filePath =>
+            {
+                var subItem = new TreeViewItem()
+                {
+                    Header = GetFileFolderName(filePath),
+                    Tag = filePath
+                };
+
+                item.Items.Add(subItem);
+            });
+
+            #endregion  
+        }
+        #endregion
+        public static string GetFileFolderName(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                return string.Empty;
+            }
+
+            var normalizedPath = path.Replace("/", "\\");
+
+            var lastIndex = normalizedPath.LastIndexOf("\\");
+
+            if (lastIndex <= 0)
+            {
+                return path;
+            }
+
+            return path.Substring(lastIndex + 1);
+        }
+    }
+}
---
+++ Helpers/Browser/HeaderToImageConverter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using UIFramework;
+
+namespace BIMiconToolbar.Helpers.Browser
+{
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var path = (string)value;
+
+            if (path == null)
+            {
+                return null;
+            }
+
+            var name = BrowserWindow.GetFileFolderName(path);
+
+            // File icon by default
+            var image = "file.png";
+
+            // Blank name, assumed to be a drive
+            if (string.IsNullOrEmpty(name))
+            {
+                image = "harddrive.png";
+            }
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+            {
+                image = "folder.png";
+            }
+            else if (name.Contains(".dwg"))
+            {
+                image = "dwg.png";
+            }
+            else if (name.Contains(".jpg"))
+            {
+                image = "jpg.png";
+            }
+            else if (name.Contains(".pdf"))
+            {
+                image = "pdf.png";
+            }
+            else if (name.Contains(".rfa"))
+            {
+                image = "revit.png";
+            }
+            else if (name.Contains(".rte"))
+            {
+                image = "revit.png";
+            }
+            else if (name.Contains(".rvt"))
+            {
+                image = "revit.png";
+            }
+            else if (name.Contains(".txt"))
+            {
+                image = "txt.png";
+            }
+            else if (name.Contains(".xls") || name.Contains(".xlsx"))
+            {
+                image = "xls.png";
+            }
+
+            return new BitmapImage(new Uri($"pack://application:,,,/BIMiconToolbar;component/Helpers/Browser/Images/{image}"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
