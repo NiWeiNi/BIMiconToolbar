40ef0caf9bc553a3147225514ab2444b0de21d70
NiWeiNi <weinini007@gmail.com>
2020-11-06 23:03:17 +1100

    feat: placeholder for export schedules owkring

---
+++ ExportSchedules/BrowserCheckboxes.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Windows;
+
+namespace BIMiconToolbar.ExportSchedules
+{
+    /// <summary>
+    /// Interaction logic for BrowserCheckboxes.xaml
+    /// </summary>
+    public partial class BrowserCheckboxes : Window, IDisposable
+    {
+        public BrowserCheckboxes(ExternalCommandData commandData)
+        {
+            InitializeComponent();
+        }
+
+        public void Dispose()
+        {
+            this.Close();
+        }
+    }
+}
--- ExportSchedules/ExportSchedules.cs
+++ ExportSchedules/ExportSchedules.cs
@@ -1,6 +1,12 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.Streaming;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace BIMiconToolbar.ExportSchedules
 {
@@ -9,7 +15,54 @@ namespace BIMiconToolbar.ExportSchedules
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            TaskDialog.Show("Warning", "Sorry, the tool is WIP");
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            // Export destination folder
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string excelPath = desktopPath + @"\Schedule.xlsx";
+
+            // Collect schedules
+            FilteredElementCollector viewsCollector = new FilteredElementCollector(doc)
+                                                    .OfCategory(BuiltInCategory.OST_Schedules);
+
+            List<ViewSchedule> schedules = viewsCollector.Cast<ViewSchedule>().Where(sh =>
+                                   sh.Name != "<Revision Schedule>").ToList();
+
+            // Extract data from schedule
+            ViewSchedule sched = schedules[0] as ViewSchedule;
+            TableData tD = sched.GetTableData();
+            TableSectionData sectionData = tD.GetSectionData(SectionType.Body);
+            int numbRows = sectionData.NumberOfRows;
+            int numbCols = sectionData.NumberOfColumns;
+
+            // Create excel file
+            SXSSFWorkbook workbook = new SXSSFWorkbook();
+            SXSSFSheet excelSheet = (SXSSFSheet)workbook.CreateSheet("Sheet1");
+            excelSheet.SetRandomAccessWindowSize(100);
+
+            //Create a header row
+            IRow row = excelSheet.CreateRow(0);
+
+            // Write to excel
+            using (var fs = new FileStream(excelPath, FileMode.Create, FileAccess.Write))
+            {
+                // Write content
+                for (int i = 0; i < numbRows; i++)
+                {
+                    row = excelSheet.CreateRow(i + 1);
+
+                    for (int j = 0; j < numbCols; j++)
+                    {
+                        string content = sched.GetCellText(SectionType.Body, i, j);
+                        row.CreateCell(j).SetCellValue(content);
+                    }
+                }
+
+                // Write to file
+                workbook.Write(fs);
+
+                TaskDialog.Show("Success", "Warnings report created in: " + excelPath);
+            }
 
             return Result.Succeeded;
         }
