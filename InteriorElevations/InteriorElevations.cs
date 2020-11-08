82b15350a667133e753d0d49872bb942573c963d
NiWeiNi <weinini007@gmail.com>
2020-11-09 00:48:53 +1100

    feat: create interior elevations and place on sheet

--- InteriorElevations/InteriorElevations.cs
+++ InteriorElevations/InteriorElevations.cs
@@ -3,6 +3,7 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
 using System.Collections.Generic;
+using System.Windows.Controls;
 
 namespace BIMiconToolbar.InteriorElevations
 {
@@ -15,15 +16,64 @@ namespace BIMiconToolbar.InteriorElevations
 
             // Variables to store user input
             List<int> selectedIntIds;
+            Element titleBlock;
+            View viewTemplate;
+            ViewFamilyType viewFamilyType;
 
             // Prompt window to collect user input
             using (InteriorElevationsWindow customWindow = new InteriorElevationsWindow(commandData))
             {
                 customWindow.ShowDialog();
                 selectedIntIds = customWindow.IntegerIds;
+                titleBlock = customWindow.SelectedComboItemTitleBlock.Tag as Element;
+                viewTemplate = customWindow.SelectedComboItemViewTemplate.Tag as View;
+                viewFamilyType = customWindow.SelectedComboItemViewType.Tag as ViewFamilyType;
             }
 
-            if (selectedIntIds != null)
+            // No required elements loaded
+            if (titleBlock == null && viewTemplate == null && viewFamilyType == null)
+            {
+                TaskDialog.Show("Warning", "Please load a Elevation, a Title Block and create a View Template");
+                return Result.Cancelled;
+            }
+            // No title block and elevation loaded
+            else if (titleBlock == null && viewFamilyType == null)
+            {
+                TaskDialog.Show("Warning", "Please load a Elevation and a Title Block");
+                return Result.Cancelled;
+            }
+            // No title block and view template
+            else if (titleBlock == null && viewTemplate == null)
+            {
+                TaskDialog.Show("Warning", "Please load a Title Block and create a View Template");
+                return Result.Cancelled;
+            }
+            // No elevation and view template
+            else if (viewFamilyType == null && viewTemplate == null)
+            {
+                TaskDialog.Show("Warning", "Please load an Elevation and create a View Template");
+                return Result.Cancelled;
+            }
+            // No elevation
+            else if (viewFamilyType == null)
+            {
+                TaskDialog.Show("Warning", "Please load an Elevation");
+                return Result.Cancelled;
+            }
+            // No title block
+            else if (titleBlock == null)
+            {
+                TaskDialog.Show("Warning", "Please load a Title Block");
+                return Result.Cancelled;
+            }
+            // No view template
+            else if (titleBlock == null)
+            {
+                TaskDialog.Show("Warning", "Please create a view template");
+                return Result.Cancelled;
+            }
+            // Room selected
+            else if (selectedIntIds != null)
             {
                 // Collect rooms
                 foreach(int id in selectedIntIds)
@@ -35,13 +85,32 @@ namespace BIMiconToolbar.InteriorElevations
 
                     if (boundaries != null)
                     {
-                        List<XYZ> points = Helpers.Helpers.BoundaPoints(boundaries);
-                        XYZ centroid = Helpers.Helpers.Centroid(points); 
+                        // Transaction
+                        Transaction t = new Transaction(doc, "Create Interior Elevations");
+                        t.Start();
 
-                        if (boundaries.Count == 4)
+                        if (boundaries.Count == 1)
                         {
-                    
+                            List<XYZ> points = Helpers.Helpers.BoundaPoints(boundaries);
+                            XYZ centroid = Helpers.Helpers.Centroid(points);
+
+                            // Create sheet
+                            ViewSheet sheet = ViewSheet.Create(doc, titleBlock.Id);
+                            sheet.Name = sheet.SheetNumber + " - " + "INTERIOR ELEVATIONS";
+
+                            // Create elevation marker
+                            ElevationMarker marker = ElevationMarker.CreateElevationMarker(doc, viewFamilyType.Id, centroid, 5);
+
+                            for (int i = 0; i < 4; i++)
+                            {
+                                View view = marker.CreateElevation(doc, doc.ActiveView.Id, i);
+                                view.ViewTemplateId = viewTemplate.Id;
+                                Viewport.Create(doc, sheet.Id, view.Id, new XYZ());
+                            }
                         }
+
+                        // Commit transaction
+                        t.Commit();
                     }
                 }
 
