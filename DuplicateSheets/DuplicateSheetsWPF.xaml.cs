15e4cbee2876b9a50a2e244c6f30c10113790997
NiWeiNi <weinini007@gmail.com>
2021-01-18 12:54:17 +1000

    feat: wpf for duplicate sheets

---
+++ DuplicateSheets/DuplicateSheetsWPF.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BIMiconToolbar.DuplicateSheets
+{
+    /// <summary>
+    /// Interaction logic for DuplicateSheetsWPF.xaml
+    /// </summary>
+    public partial class DuplicateSheetsWPF : Window
+    {
+        public DuplicateSheetsWPF()
+        {
+            InitializeComponent();
+        }
+    }
+}
