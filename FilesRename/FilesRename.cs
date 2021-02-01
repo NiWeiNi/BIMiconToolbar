513f6fa5b847b04dc6e02a8612c35e56f21c4ad9
NiWeiNi <weinini007@gmail.com>
2021-02-01 16:01:24 +1000

    feat: wpf window

--- FilesRename/FilesRename.cs
+++ FilesRename/FilesRename.cs
@@ -1,6 +1,8 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using BIMiconToolbar.Helpers.Browser;
+using System.IO;
 
 namespace BIMiconToolbar.FilesRename
 {
@@ -9,6 +11,25 @@ namespace BIMiconToolbar.FilesRename
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
+            using (BrowserWindow browserWindow = new BrowserWindow())
+            {
+                browserWindow.ShowDialog();
+
+                string fullPath = browserWindow.selectedPath;
+
+                // CHeck that path is not empty and path is a folder
+                if (fullPath == null || !Directory.Exists(fullPath))
+                {
+                    TaskDialog.Show("Warning", "No folder has been selected");
+                    return Result.Cancelled;
+                }
+                // Show next window for user input
+                else
+                {
+                    
+                }
+            }
+
             return Result.Succeeded;
         }
     }
---
+++ FilesRename/FilesRenameWPF.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BIMiconToolbar.FilesRename
+{
+    /// <summary>
+    /// Interaction logic for FilesRenameWPF.xaml
+    /// </summary>
+    public partial class FilesRenameWPF : Window
+    {
+        public FilesRenameWPF()
+        {
+            InitializeComponent();
+        }
+    }
+}
