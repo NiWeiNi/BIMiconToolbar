60bc6feebcee0fd08119c0fc6f774bfbcc65b6dc
NiWeiNi <weinini007@gmail.com>
2021-02-02 16:15:29 +1000

    feat: add helper functions for NumberBySpline

---
+++ Helpers/HelpersSelection.cs
@@ -0,0 +1,60 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+namespace BIMiconToolbar.Helpers
+{
+    class HelpersSelection
+    {
+		/// <summary>
+		/// Function for user to pick up line
+		/// </summary>
+		/// <param name="uidoc"></param>
+		/// <returns></returns>
+		public Curve PickLine(UIDocument uidoc)
+		{
+			Document doc = uidoc.Document;
+
+			// Selection filter
+			ISelectionFilter selFilter = new LineSelectionFilter();
+
+			try
+			{
+				// Prompt user to select curve that intersects with rooms
+				ElementId curveId = uidoc.Selection.PickObject(ObjectType.Element, selFilter).ElementId;
+
+				// Retrieve model curve
+				CurveElement eCurve = doc.GetElement(curveId) as CurveElement;
+				Curve curve = eCurve.GeometryCurve as Curve;
+
+				// TaskDialog.Show("Curve Picked", curve.Name);
+				return curve;
+			}
+			catch (Autodesk.Revit.Exceptions.OperationCanceledException e)
+			{
+				TaskDialog.Show("Operation Canceled", e.Message);
+				return null;
+			}
+		}
+
+		/// <summary>
+		/// Class filter to pick up only lines
+		/// </summary>
+		public class LineSelectionFilter : ISelectionFilter
+		{
+			public bool AllowElement(Element element)
+			{
+				if (element.Category.Name == "Lines")
+				{
+					return true;
+				}
+				return false;
+			}
+			// Overrride AllowReference method
+			public bool AllowReference(Reference refer, XYZ point)
+			{
+				return false;
+			}
+		}
+	}
+}
--- NumberBySpline/NumberBySpline.cs
+++ NumberBySpline/NumberBySpline.cs
@@ -1,6 +1,8 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BIMiconToolbar.NumberBySpline
 {
@@ -9,6 +11,19 @@ namespace BIMiconToolbar.NumberBySpline
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
+			Document doc = commandData.Application.ActiveUIDocument.Document;
+			UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+			string nameLevel = "Level 1";
+			// Retrieve specified level
+			FilteredElementCollector collector = new FilteredElementCollector(doc);
+			ICollection<Element> levels = collector.OfClass(typeof(Level)).ToElements();
+			var level = collector.Where(x => x.Name == nameLevel).FirstOrDefault() as Level;
+
+			// Retrieve rooms from document
+			var rooms = new FilteredElementCollector(doc)
+						.OfCategory(BuiltInCategory.OST_Rooms)
+						.Where(x => x.LevelId == level.LevelId);
 
             return Result.Succeeded;
         }
