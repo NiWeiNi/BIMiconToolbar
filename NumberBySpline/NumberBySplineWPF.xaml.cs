757033ebc76ba8019353618ea0a6c81e6a709c22
NiWeiNi <weinini007@gmail.com>
2021-03-09 18:19:40 +1000

    feat: add wpf UI

---
+++ NumberBySpline/NumberBySplineWPF.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace BIMiconToolbar.NumberBySpline
+{
+    /// <summary>
+    /// Interaction logic for NumberBySplineWPF.xaml
+    /// </summary>
+    public partial class NumberBySplineWPF : Window
+    {
+        public NumberBySplineWPF()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectSpline_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
