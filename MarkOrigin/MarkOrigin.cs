7ebdfecfcd9a4a180f618f4081543523193686e0
NiWeiNi <weinini007@gmail.com>
2021-05-26 14:30:47 +1000

    fix: filter views if not possible to place marker

--- MarkOrigin/MarkOrigin.cs
+++ MarkOrigin/MarkOrigin.cs
@@ -1,7 +1,8 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using System;
+using System.Linq;
+using BIMiconToolbar.Helpers.MessageWindow;
 
 namespace BIMiconToolbar.MarkOrigin
 {
@@ -14,52 +15,73 @@ namespace BIMiconToolbar.MarkOrigin
 
             // Retrieve active View
             View activeView = doc.ActiveView;
+            ViewType actViewType = activeView.ViewType;
 
-            // Retieve normal to view
-            XYZ normal = activeView.ViewDirection;
+            // Array of view types excluded
+            ViewType[] viewTypes = { ViewType.Report, ViewType.Schedule, ViewType.ThreeD, ViewType.Walkthrough,
+                ViewType.Undefined};
 
-            // Define horizontal and vertical projection length
-            double length = 10;
-            double x = 0;
-            double y = 0;
-            double z = 0;
-
-            // Assing value to coordinates that are not perpendicular to view
-            if (normal.X == 0)
+            // Check current view has origin and it can be drawn on a plane
+            if (viewTypes.Contains(actViewType) == false)
             {
-                x = length;
+                // Retieve normal to view
+                XYZ normal = activeView.ViewDirection;
+
+                // Define horizontal and vertical projection length
+                double length = 10;
+                double x = 0;
+                double y = 0;
+                double z = 0;
+
+                // Assing value to coordinates that are not perpendicular to view
+                if (normal.X == 0)
+                {
+                    x = length;
+                }
+                if (normal.Y == 0)
+                {
+                    y = length;
+                }
+                if (normal.Z == 0)
+                {
+                    z = length;
+                }
+
+                // Create points coordinates
+                XYZ origin = XYZ.Zero;
+
+                XYZ p1 = new XYZ(x, y, z);
+                XYZ p2 = new XYZ(-x, -y, -z);
+                XYZ p3 = new XYZ(-x, y, -z);
+                XYZ p4 = new XYZ(x, -y, z);
+
+
+                Transaction t = new Transaction(doc, "Creteate Origin Marker");
+                t.Start();
+
+                // Create line
+                Line diagonal1 = Line.CreateBound(p1, p2);
+                Line diagonal2 = Line.CreateBound(p3, p4);
+
+                doc.Create.NewDetailCurve(activeView, diagonal1);
+                doc.Create.NewDetailCurve(activeView, diagonal2);
+
+                t.Commit();
+
+                return Result.Succeeded;
             }
-            if (normal.Y == 0)
+            else
             {
-                y = length;
+                string windowTitle = "Error";
+                string errorMessage = "ViewType not supported, please open a supported View.";
+
+                using (MessageWindowWPF customWindow = new MessageWindowWPF(windowTitle, errorMessage))
+                {
+                    customWindow.ShowDialog();
+                }
+
+                return Result.Failed;
             }
-            if (normal.Z == 0)
-            {
-                z = length;
-            }
-
-            // Create points coordinates
-            XYZ origin = XYZ.Zero;
-
-            XYZ p1 = new XYZ(x, y, z);
-            XYZ p2 = new XYZ(-x, -y, -z);
-            XYZ p3 = new XYZ(-x, y, -z);
-            XYZ p4 = new XYZ(x, -y, z);
-
-
-            Transaction t = new Transaction(doc, "Creteate Origin Marker");
-            t.Start();
-
-            // Create line
-            Line diagonal1 = Line.CreateBound(p1, p2);
-            Line diagonal2 = Line.CreateBound(p3, p4);
-
-            doc.Create.NewDetailCurve(activeView, diagonal1);
-            doc.Create.NewDetailCurve(activeView, diagonal2);
-
-            t.Commit();
-
-            return Result.Succeeded;
         }
     }
 }
