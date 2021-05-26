0c87afb9f34d956641b01b80657a26398b1a74e3
NiWeiNi <weinini007@gmail.com>
2021-05-26 14:30:19 +1000

    feat: add custom message window

---
+++ Helpers/MessageWindow/ErrorMessageWPF.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+
+namespace BIMiconToolbar.Helpers.MessageWindow
+{
+    /// <summary>
+    /// Interaction logic for MessageBoxWPF.xaml
+    /// </summary>
+    public partial class MessageWindowWPF : Window, IDisposable
+    {
+        private string _windowTitle;
+        public string WindowTitle { get { return _windowTitle; } set { _windowTitle = value; } }
+
+        private string _errorMessage;
+        public string ErrorMessage { get{return _errorMessage;} set { _errorMessage = value; } }
+
+        public MessageWindowWPF(string windowTitle, string errorMessage)
+        {
+            InitializeComponent();
+            this.Title = windowTitle;
+            TextMessage.Text = errorMessage;
+        }
+
+        /// <summary>
+        /// Method to dispose window
+        /// </summary>
+        public void Dispose()
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Method executed when OK button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
