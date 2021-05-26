77ef105632472524ba4224a395d95608583f6be0
NiWeiNi <weinini007@gmail.com>
2021-05-26 14:34:16 +1000

    fix: doc link to proper page

--- Support/Docs/Docs.cs
+++ Support/Docs/Docs.cs
@@ -7,7 +7,7 @@ namespace BIMiconToolbar.Support.Docs
     [TransactionAttribute(TransactionMode.ReadOnly)]
     class Docs : IExternalCommand
     {
-        private readonly string _docsWeb = "https://www.bimicon.com/bimicon-toolbar-documentation/";
+        private readonly string _docsWeb = "https://www.bimicon.com/bimicon-plugin/";
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             Helpers.Helpers.OpenUri(_docsWeb);
