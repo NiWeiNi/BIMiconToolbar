068acb13b1f6e17107cf585ce3b483730b6956b5
NiWeiNi <weinini007@gmail.com>
2021-05-28 14:58:44 +1000

    refactor: custom error messsages

---
+++ Helpers/MessageWindows.cs
@@ -0,0 +1,20 @@
+﻿using BIMiconToolbar.Helpers.MessageWindow;
+
+namespace BIMiconToolbar.Helpers
+{
+    class MessageWindows
+    {
+        /// <summary>
+        /// Method to display custom alert messages to user
+        /// </summary>
+        /// <param name="windowTitle"></param>
+        /// <param name="errorMessage"></param>
+        public static void AlertMessage(string windowTitle, string errorMessage)
+        {
+            using (MessageWindowWPF customWindow = new MessageWindowWPF(windowTitle, errorMessage))
+            {
+                customWindow.ShowDialog();
+            }
+        }
+    }
+}
