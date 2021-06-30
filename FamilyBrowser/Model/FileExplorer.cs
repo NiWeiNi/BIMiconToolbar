b93d7aed9f3f76e9ec700f97c92502f6f7a73e7a
NiWeiNi <weinini007@gmail.com>
2021-06-30 18:28:45 +1000

    feat: model for family browser

---
+++ FamilyBrowser/Model/FileExplorer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BIMiconToolbar.FamilyBrowser.Model
+{
+    class FileExplorer
+    {
+        /// <summary>
+        /// Method to retrieve information about files in a directory
+        /// </summary>
+        /// <param name="directory"></param>
+        /// <returns></returns>
+        public static IList<FileInfo> GetFilesFromDirectory(string directory)
+        {
+               try
+               {
+                   return (from x in Directory.GetFiles(directory) select new FileInfo(x)).ToList();
+               }
+               catch (Exception e)
+               {
+                     // TODO: Log error
+               }
+
+               return new List<FileInfo>();
+        }
+    }
+}
