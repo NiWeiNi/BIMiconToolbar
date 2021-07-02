14f270409bce8978e802cfe03e41f485c054b2d4
NiWeiNi <weinini007@gmail.com>
2021-07-02 16:31:43 +1000

    feat: add ViewModelBase class

--- Helpers/MVVM/ViewModel/ViewModelBase.cs
+++ Helpers/MVVM/ViewModel/ViewModelBase.cs
@@ -1,15 +1,39 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace BIMiconToolbar.Helpers.MVVM.ViewModel
 {
     /// <summary>
     /// A base view model class that implements property changed
     /// </summary>
-    public class ViewModelBase : INotifyPropertyChanged
+    public abstract class ViewModelBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         /// <summary>
-        /// Event fired when any child property changes its value
+        /// Event fired when a property value is changed to no null
         /// </summary>
-        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Method to set property and raise property changed event 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="storage"></param>
+        /// <param name="value"></param>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+                return false;
+            storage = value;
+            this.OnPropertyChanged(propertyName);
+            return true;
+        }
     }
 }
