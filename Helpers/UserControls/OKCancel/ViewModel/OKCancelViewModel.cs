b05d33aad3de727c73adb7983201a03653cb5679
NiWeiNi <weinini007@gmail.com>
2021-07-10 18:55:44 +1000

    reafactor: organize OKCancel user control

---
+++ Helpers/UserControls/OKCancel/ViewModel/OKCancelViewModel.cs
@@ -0,0 +1,24 @@
+﻿using BIMiconToolbar.Helpers.MVVM.ViewModel;
+using System.Windows.Input;
+
+namespace BIMiconToolbar.Helpers.UserControls.OKCancel.ViewModel
+{
+    class OKCancelViewModel : ViewModelBase
+    {
+        private readonly ICommand _okExecute;
+        public ICommand OKExecute
+        {
+            get { return _okExecute; }
+        }
+
+        public void OKButtonPress()
+        {
+
+        }
+
+        public OKCancelViewModel()
+        {
+            _okExecute = new RelayCommand(() => OKButtonPress());
+        }
+    }
+}
