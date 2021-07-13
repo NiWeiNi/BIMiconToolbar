3bcbbc5bbd923644711996374283663dd465c66d
NiWeiNi <weinini007@gmail.com>
2021-07-13 12:30:36 +1000

    refactor: remove unused libraries

---
+++ Helpers/UserControls/SelectFileReferences/SelectFileLinkTypes.xaml.cs
@@ -0,0 +1,20 @@
+﻿using BIMiconToolbar.Helpers.UserControls.SelectFileReferences.Model;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace BIMiconToolbar.Helpers.UserControls.SelectFileReferences
+{
+    /// <summary>
+    /// Interaction logic for SelectFileLinkTypes.xaml
+    /// </summary>
+    public partial class SelectFileLinkTypes : UserControl
+    {
+        public ObservableCollection<SelectFileReferencesModel> FileReferences { get; set; }
+
+        public SelectFileLinkTypes()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
