f290446dfc259da41e5222bb086e41b9ec7fac89
BIMicon/Wei <weini@bimicon.com>
2023-04-01 11:48:43 +1000

    feature: preprocess warning dialogs

---
+++ Helpers/RevitTransaction.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.DB;
+using BIMicon.BIMiconToolbar.Models;
+
+namespace BIMicon.BIMiconToolbar.Helpers
+{
+    internal class RevitTransaction
+    {
+        /// <summary>
+        /// Set supression of warning dialogs that happens inside a transaction
+        /// </summary>
+        /// <param name="transaction">The transaction to set the warning dialogs supression</param>
+        public static void SetWarningDialogSupressor(Transaction transaction)
+        {
+            FailureHandlingOptions failrureHandlingOptions = transaction.GetFailureHandlingOptions();
+            failrureHandlingOptions.SetFailuresPreprocessor(new WarningsPreprocessor());
+            transaction.SetFailureHandlingOptions(failrureHandlingOptions);
+        }
+    }
+}
---
+++ Models/WarningsPreprocessor.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.Revit.DB;
+using System.Collections.Generic;
+
+namespace BIMicon.BIMiconToolbar.Models
+{
+    /// <summary>
+    /// Class to handle warnings before it is displayed in the Revit interface
+    /// </summary>
+    internal class WarningsPreprocessor : IFailuresPreprocessor
+    {
+        public FailureProcessingResult PreprocessFailures(FailuresAccessor failuresAccessor)
+        {
+            IList<FailureMessageAccessor> failures = failuresAccessor.GetFailureMessages();
+
+            foreach (FailureMessageAccessor failure in failures)
+            {
+                FailureSeverity failureSeverity = failuresAccessor.GetSeverity();
+
+                if (failureSeverity == FailureSeverity.Warning)
+                    failuresAccessor.DeleteWarning(failure);
+                else
+                    return FailureProcessingResult.ProceedWithRollBack;
+            }
+            return FailureProcessingResult.Continue;
+        }
+    }
+}
