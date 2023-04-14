45f3cd7bbcd503955dc583170623b29d230840dc
BIMicon/Wei <weini@bimicon.com>
2023-04-15 00:16:08 +1000

    checkpoint

---
+++ Models/Entitiy.cs
@@ -0,0 +1,8 @@
+﻿namespace BIMicon.BIMiconToolbar.Models
+{
+    internal class Entitiy
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Models/TreeView.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace BIMicon.BIMiconToolbar.Models
+{
+    internal class TreeView : INotifyPropertyChanged
+    {
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
