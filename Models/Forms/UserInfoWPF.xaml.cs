223c1feca0a8301a440ff4cd2b29f34f41e1125b
BIMicon/Wei <weini@bimicon.com>
2023-06-04 13:48:12 +1000

    feature: user info window

---
+++ Models/Forms/UserInfoWPF.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace BIMicon.BIMiconToolbar.Models.Forms
+{
+    /// <summary>
+    /// Interaction logic for ResultWindowWPF.xaml
+    /// </summary>
+    public partial class UserInfoWPF : Window
+    {
+        public UserInfoWPF(string title, string message)
+        {
+            InitializeComponent();
+            this.Title = title;
+            this.message.Text = message;
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
